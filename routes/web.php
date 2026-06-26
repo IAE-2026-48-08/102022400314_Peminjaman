0f7d67cf06928eb0996e6bbb134f3cfbd90abc46
102022400314 <rosirahmawati73@gmail.com>
2026-06-26 13:52:55 +0700

    update fix

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/docs/api-docs.json', function () {
+    return response()->file(storage_path('api-docs/api-docs.json'));
+});
+
+Route::get('/docs/api-docs.yaml', function () {
+    return response()->file(storage_path('api-docs/api-docs.yaml'));
+});
+
